802d43e4be3e0579b59f132ab60bcb74a8dd513a
Ian Vomero <ivomero@ymail.com>
2021-10-25 12:42:23 -0400

    added edit super hero feature

--- Superhero_project/superheroes/views.py
+++ Superhero_project/superheroes/views.py
@@ -1,3 +1,4 @@
+from typing import ContextManager
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -36,3 +37,23 @@ def create(request):
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+
+def edit(request, hero_id):
+    edit_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'edit_hero': edit_hero
+    }
+    if request.method == "POST":
+        # use form to update propeties of edit_hero
+        edit_hero.name = request.POST.get('name')
+        edit_hero.alter_ego = request.POST.get('alter_ego')
+        edit_hero.primary = request.POST.get('primary')
+        edit_hero.secondary = request.POST.get('secondary')
+        edit_hero.catchphrase = request.POST.get('catchphrase')
+
+        edit_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+
+    else:
+        return render(request, 'superheros/edit.html', context)
